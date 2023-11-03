696029bc5bfeb0d19b70958138b71aed6fa4ae21
abderrahman haouate <abderrahmanehaouate@gmail.com>
2023-11-03 10:32:44 +0100

    Entity Admin

--- src/main/java/com/youcode/marjan/Models/Admin.java
+++ src/main/java/com/youcode/marjan/Models/Admin.java
@@ -1,4 +1,51 @@
 package com.youcode.marjan.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    //Getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
